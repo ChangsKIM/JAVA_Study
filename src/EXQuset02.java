dd26372343e50f461a005b85869f96f87dc34d34
Changs <kcs7601@gmail.com>
2024-10-13 03:00:34 +0900

    알고리즘 문제01 클래스 코드 작성

--- src/EXQuset02.java
+++ src/EXQuset02.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class EXQuset02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int number = sc.nextInt();
+
+        int sum = 0;
+        while(number != 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
 
     }
 }
