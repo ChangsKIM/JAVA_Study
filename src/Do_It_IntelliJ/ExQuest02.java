e1a803af0da96d727424cc3f05d456cf8c9d637b
Changs <kcs7601@gmail.com>
2024-10-13 03:05:16 +0900

    알고리즘 문제02 클래스 생성

--- src/Do_It_IntelliJ/ExQuest02.java
+++ src/Do_It_IntelliJ/ExQuest02.java
@@ -1,7 +1,19 @@
 package Do_It_IntelliJ;
 
+import java.util.Scanner;
+
 public class ExQuest02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int number = sc.nextInt();
+
+        int sum = 0;
+        while(number != 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
 
     }
 }
