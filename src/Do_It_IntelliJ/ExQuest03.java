7ca76f3dc26a22f5059b7cefc5d43b4fafaaa542
Galaxy <kcs7601@gmail.com>
2024-10-16 01:03:42 +0900

    커밋 및 푸쉬

--- src/Do_It_IntelliJ/ExQuest03.java
+++ src/Do_It_IntelliJ/ExQuest03.java
@@ -2,6 +2,9 @@ package Do_It_IntelliJ;
 
 public class ExQuest03 {
     public static void main(String[] args) {
-
+        int a=1, b=2, c=3;
+        int sum=0;
+        sum = a+b+c;
+        System.out.println(sum);
     }
 }
